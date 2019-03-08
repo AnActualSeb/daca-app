2b2bb72cd1a849343cb08b1dedff8945775523fe
Sebastian Torres Retana <storres2@uw.edu>
2019-03-07 22:24:45 -0800

    Reorganized File Structure + Added react-sidenav component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MainForm from "./components/MainForm";
+import { Container } from "semantic-ui-react";
+import { RenderItems } from "./RenderItems";
+import { AppContainer, Navigation, Body, Title } from "./containers";
+
+class App extends Component {
+  render() {
+    return (
+      // <Container textAlign='left'>
+      //   <MainForm />
+      // </Container>
+      <RenderItems />
+    );
+  }
+}
+
+export default App;
