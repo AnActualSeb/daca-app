0f71a8c1e201ed320b140697f18bc629cc51150a
Timothy Sun <tsun1@uw.edu>
2019-03-12 19:55:21 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import YourInfo from "./YourInfo";
 import Container from "react-bootstrap/Container";
-import SideNavBar from "./SideNavBar";
+// import SideNavBar from "./SideNavBar";
 import FormTest from "./FormTest";
 import ReasonFor from "./I765/ReasonFor";
 import LastArrival from "./I765/LastArrival";
@@ -11,46 +11,32 @@ import EligibilityCat from "./I765/EligibilityCat";
 import NoHelpBeforeInterpreter from "./I765/NoHelpBeforeInterpreter";
 import NoHelpBeforePreparer from "./I765/NoHelpBeforePreparer";
 import Resume from "./Resume";
-import SidebarExampleSidebar from "./SidebarExampleSidebar";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <Container>
-//         {/* <FormTest /> */}
-//         <SideNavBar />
-//         <YourInfo />
-//         {/* <ReasonFor /> */}
-//         {/* <LastArrival /> */}
-//         {/* <EligibilityCat /> */}
-//         {/* <NoHelpBeforeInterpreter /> */}
-//         {/* <NoHelpBeforePreparer /> */}
-//         {/* <Resume /> */}
-//       </Container>
-//     );
-//   }
-// }
 import WrapUp from "./I765/WrapUp";
 import Overview from "./Overview";
 import StartI765 from "./I765/StartI765";
 import FinancialInfo from "./I765WS/FinancialInfo";
+import StartI765Ws from "./I765WS/StartI765Ws";
+import Print from "./Print";
 
 class App extends Component {
   render() {
     return (
       <Container>
-        {/* <FormTest /> */}
-        {/* <YourInfo /> */}
-        {/* <ReasonFor /> */}
-        {/* <LastArrival /> */}
-        {/* <EligibilityCat /> */}
-        {/* <NoHelpBeforeInterpreter /> */}
-        {/* <NoHelpBeforePreparer /> */}
-        {/* <Resume /> */}
-        {/* <WrapUp /> */}
-        {/* <Overview /> */}
-        {/* <StartI765 /> */}
+        <Print />
+        <FormTest />
+        <YourInfo />
+        <ReasonFor /> */}
+        <LastArrival />
+        <EligibilityCat />
+        <NoHelpBeforeInterpreter />
+        <NoHelpBeforePreparer />
+        <Resume />
+        <WrapUp />
+        <Overview />
+        <StartI765 />
         <FinancialInfo />
+        {/* <SideNavBar /> */}
+        <StartI765Ws />
       </Container>
     );
   }
---
+++ src/Print.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+
+class Print extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>Let’s print your application</h1>
+        <p>
+          You’re ready to print and sign your DACA application. We’ll also
+          include additional instructions for what you need to do before mailing
+          your application to USCIS. We will also email a copy to you so you can
+          keep it for reference.
+          <br />
+          <br /> From all of us at Junto, we wish you a smooth and successful
+          DACA application process.{" "}
+        </p>
+
+        <Button variant="primary" type="submit">
+          Download and print
+        </Button>
+        <br />
+        <br />
+        <Button variant="light" type="submit">
+          Back
+        </Button>
+
+        <br />
+      </Container>
+    );
+  }
+}
+
+export default Print;
