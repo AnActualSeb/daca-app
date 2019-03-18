ebb6e384ee754041546b536b71370bd5f551c754
Timothy Sun <tsun1@uw.edu>
2019-03-18 13:47:05 -0700

    Updates

---
+++ precache-manifest.5f6a8bf8384d9348b1d38d201d9310c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "233aaa9d32dc73dbbb387933fd4697e8",
+    "url": "/daca-app/static/media/review.233aaa9d.png"
+  },
+  {
+    "revision": "8954fd9f8e3bf08afa950f9b78f29e9e",
+    "url": "/daca-app/static/media/money.8954fd9f.png"
+  },
+  {
+    "revision": "1fb6a8aff7db6bb26cf2a3fda9fe84e0",
+    "url": "/daca-app/static/media/check.1fb6a8af.png"
+  },
+  {
+    "revision": "c874522632a2192bef83",
+    "url": "/daca-app/static/js/runtime~main.095cce56.js"
+  },
+  {
+    "revision": "5f1baef63d0cc5026503",
+    "url": "/daca-app/static/js/main.14f8a319.chunk.js"
+  },
+  {
+    "revision": "c9e67d8c6ae0a4ea3400",
+    "url": "/daca-app/static/js/2.d787bb23.chunk.js"
+  },
+  {
+    "revision": "5f1baef63d0cc5026503",
+    "url": "/daca-app/static/css/main.5b79b735.chunk.css"
+  },
+  {
+    "revision": "c9e67d8c6ae0a4ea3400",
+    "url": "/daca-app/static/css/2.07cfc93e.chunk.css"
+  },
+  {
+    "revision": "69ed942efb8bce4f024bda36ca9df73f",
+    "url": "/daca-app/index.html"
+  }
+];
